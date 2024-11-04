30dc4a31dd56212b4c9e313fdf99cf5a1fe74ba4
Nadine <nadine@bayzat.com>
2024-11-04 22:57:59 +0400

    Add a content file

--- client/src/content.js
+++ client/src/content.js
@@ -1,4 +1,52 @@
-export const newsHeadlines = {
-  title: "Latest News",
-  buttonTitle: "Read More" 
+export const landingPage = {
+  NewsHeadlinesTitle: "Trending"
+}
+
+
+export const pageNotFound = {
+  title: "404 - Page Not Found",
+  description: "Sorry, the page you are looking for does not exist.",
+  buttonTitle: "Go Back to Home Page",
+  url: "/"
+}
+
+export const error = {
+  errorMessage: "Something went wrong",
+  buttonTitle: "Tap to retry"
+}
+
+export const categories = [
+  {
+    id: "general",
+    title: "General"
+  },
+  {
+    id: "business",
+    title: "Business"
+  },
+  {
+    id: "entertainment",
+    title: "Entertainment"
+  },
+  {
+    id: "health",
+    title: "Health"
+  },
+  {
+    id: "science",
+    title: "Science"
+  },
+  {
+    id: "sports",
+    title: "Sports"
+  },
+  {
+    id: "technology",
+    title: "Technology"
+  }
+]
+
+export const noNews = {
+  title:  "No news available.",
+  subtitle: "Please select another category."
 }
