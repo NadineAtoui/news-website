bd34c3388a92d77efe7fe6bc6fae200fd52caa8e
Nadine <nadine@bayzat.com>
2024-11-04 23:11:04 +0400

    Main page for fetching and displaying data

--- client/src/pages/landingPage/index.tsx
+++ client/src/pages/landingPage/index.tsx
@@ -1,26 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './LandingPage.module.css';
-import { LandingPageProps } from './LandingPage.types'; 
 import NewsHeadlines from '../../components/newsHeadlines'
-import useFetchApi from 'hooks/useFetchApi';
+import useFetchApi from '../../hooks/useFetchApi';
 import { fetchNewsHeadlines } from '../../api/news_healines';
 import { NewsApiResponse } from '../../types'
-import {newsHeadlines} from '../../content'
+import { formatDate } from '../../helpers';
+import MainHeadline from '../../components/mainHeadline'
+import Error from '../../components/error';
+import { error, categories, landingPage, noNews } from "../../content"
+import Loader from '../../components/loader';
+import ArticleCategories from '../../components/articleCategories'
+import NoNews from '../../components/noNews';
 
-const LandingPage: React.FC<LandingPageProps> = () => {
+const LandingPage: React.FC = () => {
+  const [selectedCategory, setSelectedCategory] = useState<string>(
+    () => localStorage.getItem("selectedCategory") || "general"
+  );
 
-  const headlines = useFetchApi<NewsApiResponse>(fetchNewsHeadlines);
+  useEffect(() => {
+    localStorage.setItem("selectedCategory", selectedCategory);
+  }, [selectedCategory]);
 
-  // TODO present loading and error in encapsulated components
-  if (headlines.loading) return <div>Loading...</div>;
-  if (headlines.error) return <div>Error: {headlines.error}</div>;
+  const headlines = useFetchApi<NewsApiResponse, string>(fetchNewsHeadlines, selectedCategory);
 
-  const headlinesContent = headlines.data?.articles || [];
-  
+  const handleCategorySelect = (category: string) => {
+    setSelectedCategory(category);
+  };
+
+  // Choose only articles that have images & description and format their date
+  const filterAndFormatArticles = (articles: NewsApiResponse['articles'] = []) => 
+    articles
+      .filter(article => article.urlToImage && article.description) 
+      .map(article => ({
+        ...article,
+        publishedAt: formatDate(article.publishedAt)
+      }));
+      
+  const articlesWithImagesAndDescription = filterAndFormatArticles(headlines.data?.articles);
+  const mainArticle = articlesWithImagesAndDescription[0];
+
   return (
-    <div className={styles.container}>
-      <NewsHeadlines title={newsHeadlines.title} headlines={headlinesContent} buttonTitle={newsHeadlines.buttonTitle}/>
-    </div>
+    <>
+      <div className={styles.container}>
+        <ArticleCategories categories={categories} onCategorySelect={handleCategorySelect} />
+
+        { headlines.loading?
+          <Loader />
+          : 
+          ( headlines.error ?
+            <Error errorMessage={error.errorMessage} buttonTitle={error.buttonTitle} onClick={headlines.retry}/>
+            :
+            (articlesWithImagesAndDescription.length > 0?
+              <div className={styles.headlines}>
+                <div className={styles.headlinesListContainer}>
+                  <h2>{landingPage.NewsHeadlinesTitle}</h2>
+                  <NewsHeadlines articlesWithImage={articlesWithImagesAndDescription.slice(1)}/>
+                </div>
+                
+                <div className={styles.mainHeadlineContainer}>
+                  <MainHeadline
+                    article={{
+                      articleUrl: mainArticle.url,
+                      title: mainArticle.title,
+                      description: mainArticle.description!,
+                      source: mainArticle.source.name,
+                      publishDate: mainArticle.publishedAt,
+                      imageUrl: mainArticle.urlToImage!,
+                    }}
+                  />
+                </div>
+              </div>
+              : 
+              <NoNews content={noNews}/>
+            )
+          )
+        }
+      </div>
+    </>
   );
 };
 
