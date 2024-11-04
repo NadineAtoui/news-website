e76055e8291331cd18bf30370fdc9f57e61bbd54
Nadine <nadine@bayzat.com>
2024-11-04 22:59:34 +0400

    Add newsHeadlines component

--- client/src/components/newsHeadlines/index.tsx
+++ client/src/components/newsHeadlines/index.tsx
@@ -1,26 +1,27 @@
 import { NewsHeadlinesApi } from '../../types';
+import Headline from '../../components/headline'
+import styles from './NewsHeadlines.module.css'
 
 interface NewsHeadlinesProps {
-  title: string;
-  headlines: NewsHeadlinesApi[];
-  buttonTitle: string;
+  articlesWithImage?: NewsHeadlinesApi[];
 }
 
-const NewsHeadlines: React.FC<NewsHeadlinesProps> = ({title, buttonTitle, headlines}) => {
+const NewsHeadlines: React.FC<NewsHeadlinesProps> = ({ articlesWithImage}) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      <ul>
-        {headlines.map((headline, index) => (
-          <li key={`${headline.source.id}-${index}`}>
-            <h2>{headline.title}</h2>
-            <p>{headline.description}</p>
-            <a href={headline.url} target="_blank" rel="noopener noreferrer">
-              {buttonTitle}
-            </a>
-          </li>
-        ))}
-      </ul>
+    <div className={styles.container}>
+      {articlesWithImage && articlesWithImage.map((headline, index) => (
+        <div className={styles.headlineContainer} key={`${headline.source.id}-${index}`}>
+          <Headline
+            articleUrl={headline.url}
+            title={headline.title}
+            source={headline.source.name}
+            publishDate={headline.publishedAt}
+            number={index + 1}
+            imageUrl={headline.urlToImage!}
+            maxTitleChar={50}
+          />
+        </div>
+      ))}
     </div>
   );
 };
